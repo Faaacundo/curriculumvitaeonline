11e6bc4542ca07c8aaf2fc5b33174a07c60e4dea
Facundo Nicolás <facundo.orlando@hotmail.com>
2017-06-11 13:20:20 -0300

    Fix: agregando formulario Le vamos a agregar un formulario de contacto y abajo un mailto: para que se contacten conmigo de manera rápida.

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -47,12 +47,12 @@ $(function() {
 //            var posicion = $("#hastaaqui").offset().top;
 //            $("html, body").animate({
 //                scrollTop: posicion
-//            }, 2000); 
+//            }, 2000);
 //        });
 //        $(document).on("scroll", function(){
 //            var desplazamientoActual = $(document).scrollTop();
 //            var controlArriba = $("#irarriba");
-//            console.log("Estoy en " , desplazamientoActual); 
+//            console.log("Estoy en " , desplazamientoActual);
 //            if(desplazamientoActual > 100 && controlArriba.css("display") == "none"){
 //                controlArriba.fadeIn(500);
 //            }
@@ -64,7 +64,7 @@ $(function() {
 //            e.preventDefault();
 //            $("html, body").animate({
 //                scrollTop: 0
-//            }, 1000); 
+//            }, 1000);
 //        });
 //    });
 
@@ -81,53 +81,53 @@ $(document).ready(function(){
 });
 
 $(document).on('scroll', function() {
-    
+
   var se1 = $('#sobre-mi').position().top;
   var se2 = $('#estudios').position().top;
   var se3 = $('#empleos').position().top;
   var se4 = $('#hobbies').position().top;
 
-  
+
 // esto funcionaAAA NO TOCAR PO AS DUDAS
 // if($(this).scrollTop() >= se1){
-//       $("div a.ripple").addClass("grey"); 
+//       $("div a.ripple").addClass("grey");
 //    }
 //  else{
 //    $("div a.ripple").removeClass("grey");
 //  };
-    
+
       if($(this).scrollTop() >= se1){
-       $("a.fboton").addClass("grey"); 
+       $("a.fboton").addClass("grey");
     }
   else{
     $("a.fboton").removeClass("grey");
   };
-    
-  
-  
+
+
+
   if($(this).scrollTop() >= se2){
-       $("a.tboton").addClass("grey"); 
-      
+       $("a.tboton").addClass("grey");
+
        $("a.fboton").removeClass("grey");
     }
   else{
     $("a.tboton").removeClass("grey");
   };
-  
- 
-  
-  
+
+
+
+
   if($(this).scrollTop() >= se3){
-       $("nav").addClass("bg-3"); 
+       $("nav").addClass("bg-3");
        $("nav").removeClass("bg-2");
     }
   else{
     $("nav").removeClass("bg-3");
   };
-  
-  
+
+
   if($(this).scrollTop() >= se4){
-       $("nav").addClass("bg-4"); 
+       $("nav").addClass("bg-4");
        $("nav").removeClass("bg-3");
     }
   else{
@@ -138,18 +138,185 @@ $(document).on('scroll', function() {
 document.addEventListener('DOMContentLoaded', function(){
       Typed.new('.element', {
         strings: [
-            "Soy una persona con muchos intereses y gustos diferentes. Estoy terminando la secundaria y tengo ganas de trabajar en un lugar donde pueda aprender y crecer como profesional. Siento que podría integrarme en un equipo de trabajo y soy muy responsable en las tareas que me corresponden. <br> Creo que la programación es una herramienta muy necesaria para desarrollarse en la sociedad hoy y es un espacio donde creo que podría aportar, además de seguir aprendiendo."],
+            "Soy una persona con muchos intereses y gustos diferentes. Terminé la secundaria y tengo ganas de trabajar en un lugar donde pueda aprender y crecer como profesional. Siento poder integrarme en un equipo de trabajo y soy muy responsable en las tareas que me corresponden. <br> Creo que la programación es una herramienta muy necesaria para desarrollarse en la sociedad hoy y es un espacio donde creo que podría aportar, además de seguir aprendiendo."],
         typeSpeed: 0
       });
   });
 
 $(".element").ready(function(){
-    
 
-        setTimeout(function(){ 
-            $(".texto").append("<div class='firma'><img class='firmita' src='2.svg' alt=''><p class='aclaracion'>Orlando, Facundo Nicolás</p></div>");
+
+        setTimeout(function(){
+            $(".texto").append("<div class='firma'><img class='firmaDibujo' src='firma.svg' alt=''><p class='aclaracion'><br>Orlando, Facundo Nicolás</p></div>");
             $('.aclaracion').hide();
-            $('.aclaracion').fadeIn(5000);
-        }, 17800);
-    
+            $('.aclaracion').fadeIn(2000);
+            $('.aclaracion').css({
+                        borderTop:"1px solid #434343",
+                        position: "relative",
+                        top:"-78px",
+                        right:"10px"
+            });
+        }, 18100);
+
  });
+
+
+// FORMULARIOOOOOOOOOOOOOOOOO
+
+//VERIFICAR VARIABLES MEDIAS RARAS DEL VAL AVER IS SE PUEDE SIMPLIFICAR
+(function() {
+
+$(document).ready(function(){
+    var val = $('#nombre').val(); // Toma el valor del Input
+
+    $('#nombre').focus(function(){
+         var val = $('#nombre').val();
+
+            $(this).removeClass('error');
+            $('.form-item .label').removeClass('error');
+    });
+
+    $('#nombre').blur(function(){
+         var val = $('#nombre').val();
+
+        if(val.length<1){
+            $(this).addClass('error');
+             $('.nombre').addClass('error');
+        }
+    });
+});
+
+}())
+
+
+
+
+// Recorrer los elementos y hacer que onchange ejecute una funcion para comprobar el valor de ese input
+(function() {
+
+  var formulario = document.formulario_registro,
+    elementos = formulario.elements;
+
+  // Funcion que se ejecuta cuando el evento click es activado
+
+  var validarInputs = function() {
+    for (var i = 0; i < elementos.length; i++) {
+      // Identificamos si el elemento es de tipo texto, email, password, radio o checkbox
+      if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
+        // Si es tipo texto, email o password vamos a comprobar que esten completados los input
+        if (elementos[i].value.length == 0) {
+          console.log('El campo ' + elementos[i].name + ' esta incompleto');
+          elementos[i].className = elementos[i].className + " error";
+          return false;
+        } else {
+          elementos[i].className = elementos[i].className.replace(" error", "");
+        }
+      }
+    }
+
+    // Comprobando que las contraseñas coincidan
+    if (elementos.pass.value !== elementos.pass2.value) {
+      elementos.pass.value = "";
+      elementos.pass2.value = "";
+      elementos.pass.className = elementos.pass.className + " error";
+      elementos.pass2.className = elementos.pass2.className + " error";
+    } else {
+      elementos.pass.className = elementos.pass.className.replace(" error", "");
+      elementos.pass2.className = elementos.pass2.className.replace(" error", "");
+    }
+
+    return true;
+  };
+
+  var validarRadios = function() {
+    var opciones = document.getElementsByName('sexo'),
+      resultado = false;
+
+    for (var i = 0; i < elementos.length; i++) {
+      if (elementos[i].type == "radio" && elementos[i].name == "sexo") {
+        // Recorremos los radio button
+        for (var o = 0; o < opciones.length; o++) {
+          if (opciones[o].checked) {
+            resultado = true;
+            break;
+          }
+        }
+
+        if (resultado == false) {
+          elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+          console.log('El campo sexo esta incompleto');
+          return false;
+        } else {
+          // Eliminamos la clase Error del radio button
+          elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+          return true;
+        }
+      }
+    }
+  };
+
+  var validarCheckbox = function() {
+    var opciones = document.getElementsByName('terminos'),
+      resultado = false;
+
+    for (var i = 0; i < elementos.length; i++) {
+      if (elementos[i].type == "checkbox") {
+        for (var o = 0; o < opciones.length; o++) {
+          if (opciones[o].checked) {
+            resultado = true;
+            break;
+          }
+        }
+
+        if (resultado == false) {
+          elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
+          console.log('El campo checkbox esta incompleto');
+          return false;
+        } else {
+          // Eliminamos la clase Error del checkbox
+          elementos[i].parentNode.className = elementos[i].parentNode.className.replace(" error", "");
+          return true;
+        }
+      }
+    }
+  };
+
+  var enviar = function(e) {
+    if (!validarInputs()) {
+      console.log('Falto validar los Input');
+      e.preventDefault();
+    } else if (!validarRadios()) {
+      console.log('Falto validar los Radio Button');
+      e.preventDefault();
+    } else if (!validarCheckbox()) {
+      console.log('Falto validar Checkbox');
+      e.preventDefault();
+    } else {
+      console.log('Envia');
+      e.preventDefault();
+    }
+  };
+
+  var focusInput = function() {
+    this.parentElement.children[1].className = "label active";
+    this.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
+  };
+
+  var blurInput = function() {
+    if (this.value <= 0) {
+      this.parentElement.children[1].className = "label";
+      this.parentElement.children[0].className = this.parentElement.children[0].className + " error";
+    }
+  };
+
+  // --- Eventos ---
+  formulario.addEventListener("submit", enviar);
+
+  for (var i = 0; i < elementos.length; i++) {
+    if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
+      elementos[i].addEventListener("focus", focusInput);
+      elementos[i].addEventListener("blur", blurInput);
+    }
+  }
+
+}())
